d03d163220539af0c305a220d5b3356745bcf51d
vcky4 <okon.victor.u@gmail.com>
2024-02-24 00:17:07 +0100

    feet: crud operation on recipe service

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+import { UpdateRecipeDto } from './dto/update-recipe.dto';
 
 @Injectable()
 export class RecipeService {
     constructor(private prismaService: PrismaService) { }
 
     //create recipe
+    async create(data: CreateRecipeDto) {
+        return this.prismaService.recipe.create({
+            data,
+        });
+    }
+
+    //find all recipes
+    async findAll() {
+        return this.prismaService.recipe.findMany();
+    }
+
+    //find one recipe
+    async findOne(id: string) {
+        return this.prismaService.recipe.findUnique({
+            where: { id },
+        });
+    }
+
+    //update recipe
+    async update(id: string, data: UpdateRecipeDto) {
+        return this.prismaService.recipe.update({
+            where: { id },
+            data,
+        });
+    }
+
+    //delete recipe
+    async remove(id: string) {
+        return this.prismaService.recipe.delete({
+            where: { id },
+        });
+    }
 }
